490094035148cca8bf8f019953841fa203cc2766
dhani <dhaniverdant23@gmail.com>
2021-10-07 23:30:10 +0800

    modify app and create home mock component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello React</h2>
+      <Home />
     </div>
   );
 }
