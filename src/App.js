242dca0220d3e7470341d338724824ca21c2c136
dhani <dhaniverdant23@gmail.com>
2021-10-25 21:22:37 +0800

    create routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './pages/Home/Home';
+import UserDetail from './pages/User Detail/UserDetail';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/user/:userId" exact component={UserDetail} />
+          <Route path="*">
+            <h2>404 Not Found</h2>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/User Detail/UserDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UserDetail() {
+  return (
+    <div>
+      <h2>User Detail Page</h2>
+    </div>
+  )
+}
+
+export default UserDetail;
