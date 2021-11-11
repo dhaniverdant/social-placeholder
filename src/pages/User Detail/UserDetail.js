86f0623189e66c89ae8f4867b5e76dd83bbdcdd7
dhani <dhaniverdant23@gmail.com>
2021-11-11 09:06:58 +0800

    fetch user details and show on user detail page

--- src/pages/User Detail/UserDetail.js
+++ src/pages/User Detail/UserDetail.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router';
+import { fetchUser } from '../../redux/actions/userActions';
 
 function UserDetail() {
+  const { userId } = useParams();
+  let user = useSelector((state) => state.user);
+  const { name, username, email, phone } = user;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUser(userId));
+  }, [userId]);
+
   return (
     <div>
-      <h2>User Detail Page</h2>
+      {Object.keys(user).length === 0 ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          <h2>{name}</h2>
+          <div>{username}</div>
+          <div>{email}</div>
+          <div>{phone}</div>
+        </div>
+      )}
     </div>
   )
 }
